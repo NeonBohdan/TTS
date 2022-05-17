23266891ed707d7b72fa8274cc63923debc34eec
NeonBohdan <bohdan@neon.ai>
2022-05-17 16:01:51 +0300

    Removed ja_jp_phonemizer

--- TTS/tts/utils/text/phonemizers/__init__.py
+++ TTS/tts/utils/text/phonemizers/__init__.py
@@ -1,8 +1,7 @@
 from TTS.tts.utils.text.phonemizers.base import BasePhonemizer
 from TTS.tts.utils.text.phonemizers.espeak_wrapper import ESpeak
-from TTS.tts.utils.text.phonemizers.ja_jp_phonemizer import JA_JP_Phonemizer
 
-PHONEMIZERS = {b.name(): b for b in (ESpeak, JA_JP_Phonemizer)}
+PHONEMIZERS = {b.name(): b for b in (ESpeak,)}
 
 
 ESPEAK_LANGS = list(ESpeak.supported_languages().keys())
@@ -19,8 +18,6 @@ DEF_LANG_TO_PHONEMIZER.update(_new_dict)
 
 # Force default for some languages
 DEF_LANG_TO_PHONEMIZER["en"] = DEF_LANG_TO_PHONEMIZER["en-us"]
-DEF_LANG_TO_PHONEMIZER["ja-jp"] = JA_JP_Phonemizer.name()
-
 
 def get_phonemizer_by_name(name: str, **kwargs) -> BasePhonemizer:
     """Initiate a phonemizer by name
@@ -34,8 +31,6 @@ def get_phonemizer_by_name(name: str, **kwargs) -> BasePhonemizer:
     """
     if name == "espeak":
         return ESpeak(**kwargs)
-    if name == "ja_jp_phonemizer":
-        return JA_JP_Phonemizer(**kwargs)
     raise ValueError(f"Phonemizer {name} not found")
 
 
