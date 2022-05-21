5bdf271e21d01658b57b357522e80b17140756a4
NeonBohdan <bohdan@neon.ai>
2022-05-22 01:16:28 +0300

    Fixed unpack key_size

--- TTS/tts/layers/glow_tts/transformer.py
+++ TTS/tts/layers/glow_tts/transformer.py
@@ -123,7 +123,8 @@ class RelativePositionMultiHeadAttention(nn.Module):
 
     def attention(self, query, key, value, mask=None):
         # reshape [b, d, t] -> [b, n_h, t, d_k]
-        b, d, t_s, t_t = (*key.size(), query.size(2))
+        key_size = key.size()
+        b, d, t_s, t_t = (key_size[0], key_size[1], key_size[2], query.size(2))
         query = query.view(b, self.num_heads, self.k_channels, t_t).transpose(2, 3)
         key = key.view(b, self.num_heads, self.k_channels, t_s).transpose(2, 3)
         value = value.view(b, self.num_heads, self.k_channels, t_s).transpose(2, 3)
